6ba1c5d86c52ed498a12bce337ed4b3697ce187c
YingChengYao <632173516@qq.com>
2021-01-14 14:52:04 +0800

    添加项目文件。

---
+++ DataProtectionSample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtectionSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        IDataProtector _protector;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            IDataProtectionProvider provider)
+        {
+            _logger = logger;
+            _protector = provider.CreateProtector(GetType().FullName);
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("index")]
+        public string Index()
+        {
+            var protectedData = _protector.Protect("Hello World");
+
+            return protectedData;
+        }
+    }
+}
---
+++ DataProtectionSample/Helper/DataProtectionHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProtectionSample.Helper
+{
+    public class DataProtectionHelper
+    {
+        private readonly IDataProtectionProvider _dataProtectionProvider;
+
+        public DataProtectionHelper(IDataProtectionProvider dataProtectionProvider)
+        {
+            _dataProtectionProvider = dataProtectionProvider;
+        }
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="textToEncrypt"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string Encrypt(string textToEncrypt, string key)
+        {
+            return _dataProtectionProvider.CreateProtector(key).Protect(textToEncrypt);
+        }
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="cipherText"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string Decrypt(string cipherText, string key)
+        {
+            return _dataProtectionProvider.CreateProtector(key).Unprotect(cipherText);
+        }
+    }
+}
